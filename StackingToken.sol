32b203e48636c7b6274197282880223b300e68c2
inc <incaelum@hotmail.com>
2024-03-27 21:30:07 +0100

    ERC20 contracts

--- StackingToken.sol
+++ StackingToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract StackingToken is ERC20 {
-    constructor() ERC20("Tester", "TST") {
+    constructor() ERC20("StackingToken", "STK") {
         // Inicializa el contrato con un total de 1 millón de tokens
         _mint(msg.sender, 1000000 * 10 ** uint(decimals()));
     }
