c8afea7125df9884d924a17c9f45a0d4f5c4db13
inc <incaelum@hotmail.com>
2024-03-29 12:14:04 +0100

    fix comments

--- StackingToken.sol
+++ StackingToken.sol
@@ -1,11 +1,17 @@
 // SPDX-License-Identifier: MIT
+
+// this is just a basic implementation of any ERC20 for testing.
+// If you want to reward users for providing liquidity, you need to use as stackingToken
+// the token LP you recibe to provide liquidity in a Uniswap V2 contract.
+
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+
+
 contract StackingToken is ERC20 {
     constructor() ERC20("StackingToken", "STK") {
-        // Inicializa el contrato con un total de 1 millón de tokens
         _mint(msg.sender, 1000000 * 10 ** uint(decimals()));
     }
 }
